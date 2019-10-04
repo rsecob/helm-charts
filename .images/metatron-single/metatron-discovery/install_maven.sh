50a3fd420c600c2140feb47d1828ba26db1f39c3
Romain Boces <bocesr@gmail.com>
2019-10-04 16:45:30 -0400

    metatron: - turn discovery deployment to a statefulset - use volumeClaimTemplate for better data persistence - update maven in discovery image - bump version to 0.1.3

--- .images/metatron-single/metatron-discovery/install_maven.sh
+++ .images/metatron-single/metatron-discovery/install_maven.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-wget https://www-us.apache.org/dist/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz
-tar xf apache-maven-3.6.0-bin.tar.gz -C /opt
-ln -s /opt/apache-maven-3.6.0 /opt/maven
+wget http://mirror.dsrg.utoronto.ca/apache/maven/maven-3/3.6.2/binaries/apache-maven-3.6.2-bin.tar.gz
+tar xf apache-maven-3.6.2-bin.tar.gz -C /opt
+ln -s /opt/apache-maven-3.6.2 /opt/maven
